6fd9b060da71c95cf3ea105b18a95ecf078329f9
tristangaebler <Tristan.Gaebler@gmail.com>
2016-05-04 08:33:35 -0600

    Added contents to RegexFrame

--- src/javaRegex/view/RegexFrame.java
+++ src/javaRegex/view/RegexFrame.java
@@ -1,6 +1,34 @@
 package javaRegex.view;
 
-public class RegexFrame
-{
+import javaRegex.controller.*;
+import javax.swing.*;
+import java.awt.*;
 
+public class RegexFrame extends JFrame
+{
+	private RegexController baseController;
+	private RegexPanel basePanel;
+	
+	public RegexFrame(RegexController baseController)
+	{
+		this.baseController = baseController;
+		basePanel = new RegexPanel(baseController);
+		setUpFrame();
+	}
+	
+	private void setUpFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setSize(800, 800);
+		this.setBackground(Color.CYAN);
+		this.setTitle("Regex Window");
+		this.setResizable(true);
+		this.setVisible(true);
+		
+	}
+	
+	public RegexController getBaseController()
+	{
+		return baseController;
+	}
 }
